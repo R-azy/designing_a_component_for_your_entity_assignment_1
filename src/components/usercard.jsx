97ba2030e8a446eb609cbaeaf1c00d367cdc6198
R-azy <muhammedrazy1798@gmail.com>
2025-03-06 09:47:20 +0530

    done

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,23 @@
 // write the component code here
+import React from 'react'
+
+const usercard = () => {
+    const profilephoto = "https://imgd.aeplcdn.com/664x374/n/cw/ec/47278/m8-exterior-right-front-three-quarter-2.jpeg?isig=0&q=80" ;
+    const name = "car" ;
+    const mail = "car14@gmail.com" ;
+    const phonenumber = "9999999999" ;
+    const address = "america, california" ;
+  return (
+    <div>
+      <div>
+        <img src={profilephoto} alt="profilephoto" />
+        <h2>{name}</h2>
+        <p>{mail}</p>
+        <p>{phonenumber}</p>
+        <p>{address}</p>
+      </div>
+    </div>
+  )
+}
+
+export default usercard
